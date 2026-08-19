7abcdfa491205463bdfac66e6c8bdeb44f39ca17
crumin001-cmyk <crumin001@gmail.com>
2026-08-20 02:42:24 +0900

    code修正

--- src/app/Models/Condition.php
+++ src/app/Models/Condition.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Condition;
 
 class Condition extends Model
 {
@@ -11,11 +10,4 @@ class Condition extends Model
         'name',
         'sort_order',
     ];
-
-    public function condition()
-{
-    return $this->hasMany(Condition::class);
-}
-
-
 }
