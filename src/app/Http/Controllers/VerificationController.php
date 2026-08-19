7a5da6e2f75b0d6f996c245dfe113f6ed530545c
crumin001-cmyk <crumin001@gmail.com>
2026-08-20 02:10:19 +0900

    code修正

--- src/app/Http/Controllers/VerificationController.php
+++ src/app/Http/Controllers/VerificationController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use App\Http\Requests\verifiedRequest;
 
 class VerificationController extends Controller
 {
